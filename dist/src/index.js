b7295767180a6e6bab90804f6eeffee60a7f9d85
sogoiii <sogoiii@gmail.com>
2022-10-26 16:37:47 -0700

    update dist, update environment variable change

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,77 +1,17 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.supportedShortNetworks =
-  exports.supportedNetworks =
-  exports.getNetworkByLzId =
-  exports.getNetworkByHolographId =
-  exports.getNetworkByChainId =
-  exports.getNetworkByShortKey =
-  exports.getNetworkByKey =
-  exports.getSupportedNetworks =
-  exports.networks =
-  exports.NetworkType =
-    void 0;
-const network_type_1 = require('./network-type');
-Object.defineProperty(exports, 'NetworkType', {
-  enumerable: true,
-  get: function () {
-    return network_type_1.NetworkType;
-  },
-});
-const networks_1 = require('./networks');
-Object.defineProperty(exports, 'networks', {
-  enumerable: true,
-  get: function () {
-    return networks_1.networks;
-  },
-});
-const utils_1 = require('./utils');
-Object.defineProperty(exports, 'getSupportedNetworks', {
-  enumerable: true,
-  get: function () {
-    return utils_1.getSupportedNetworks;
-  },
-});
-Object.defineProperty(exports, 'getNetworkByKey', {
-  enumerable: true,
-  get: function () {
-    return utils_1.getNetworkByKey;
-  },
-});
-Object.defineProperty(exports, 'getNetworkByShortKey', {
-  enumerable: true,
-  get: function () {
-    return utils_1.getNetworkByShortKey;
-  },
-});
-Object.defineProperty(exports, 'getNetworkByChainId', {
-  enumerable: true,
-  get: function () {
-    return utils_1.getNetworkByChainId;
-  },
-});
-Object.defineProperty(exports, 'getNetworkByHolographId', {
-  enumerable: true,
-  get: function () {
-    return utils_1.getNetworkByHolographId;
-  },
-});
-Object.defineProperty(exports, 'getNetworkByLzId', {
-  enumerable: true,
-  get: function () {
-    return utils_1.getNetworkByLzId;
-  },
-});
-Object.defineProperty(exports, 'supportedNetworks', {
-  enumerable: true,
-  get: function () {
-    return utils_1.supportedNetworks;
-  },
-});
-Object.defineProperty(exports, 'supportedShortNetworks', {
-  enumerable: true,
-  get: function () {
-    return utils_1.supportedShortNetworks;
-  },
-});
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.supportedShortNetworks = exports.supportedNetworks = exports.getNetworkByLzId = exports.getNetworkByHolographId = exports.getNetworkByChainId = exports.getNetworkByShortKey = exports.getNetworkByKey = exports.getSupportedNetworks = exports.networks = exports.NetworkType = void 0;
+const network_type_1 = require("./network-type");
+Object.defineProperty(exports, "NetworkType", { enumerable: true, get: function () { return network_type_1.NetworkType; } });
+const networks_1 = require("./networks");
+Object.defineProperty(exports, "networks", { enumerable: true, get: function () { return networks_1.networks; } });
+const utils_1 = require("./utils");
+Object.defineProperty(exports, "getSupportedNetworks", { enumerable: true, get: function () { return utils_1.getSupportedNetworks; } });
+Object.defineProperty(exports, "getNetworkByKey", { enumerable: true, get: function () { return utils_1.getNetworkByKey; } });
+Object.defineProperty(exports, "getNetworkByShortKey", { enumerable: true, get: function () { return utils_1.getNetworkByShortKey; } });
+Object.defineProperty(exports, "getNetworkByChainId", { enumerable: true, get: function () { return utils_1.getNetworkByChainId; } });
+Object.defineProperty(exports, "getNetworkByHolographId", { enumerable: true, get: function () { return utils_1.getNetworkByHolographId; } });
+Object.defineProperty(exports, "getNetworkByLzId", { enumerable: true, get: function () { return utils_1.getNetworkByLzId; } });
+Object.defineProperty(exports, "supportedNetworks", { enumerable: true, get: function () { return utils_1.supportedNetworks; } });
+Object.defineProperty(exports, "supportedShortNetworks", { enumerable: true, get: function () { return utils_1.supportedShortNetworks; } });
 //# sourceMappingURL=index.js.map
--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,28 +1,28 @@
 import { NetworkType } from './src/network-type';
 export interface Network {
-  key: string;
-  shortKey: string;
-  type: NetworkType;
-  chain: number;
-  rpc: string;
-  holographId: number;
-  tokenName: string;
-  tokenSymbol: string;
-  explorer: string | undefined;
-  lzEndpoint: string;
-  lzId: number;
-  active: boolean;
+    key: string;
+    shortKey: string;
+    type: NetworkType;
+    chain: number;
+    rpc: string;
+    holographId: number;
+    tokenName: string;
+    tokenSymbol: string;
+    explorer: string | undefined;
+    lzEndpoint: string;
+    lzId: number;
+    active: boolean;
 }
 export interface Networks {
-  [key: string]: Network;
+    [key: string]: Network;
 }
 declare type NetworkKeys = keyof Networks;
 export interface NetworkHelper {
-  byKey: Networks;
-  byShortKey: Networks;
-  byChainId: Networks;
-  byHolographId: Networks;
-  byLzId: Networks;
+    byKey: Networks;
+    byShortKey: Networks;
+    byChainId: Networks;
+    byHolographId: Networks;
+    byLzId: Networks;
 }
 export { NetworkType, NetworkKeys };
 //# sourceMappingURL=types.d.ts.map

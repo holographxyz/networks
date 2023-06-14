d7614126c96df79e7dadb99bc6b302fb0eec91dc
sogoiii <sogoiii@gmail.com>
2023-06-13 18:53:41 -0700

    update dist folder and bump to 0.1.26

--- dist/types.js
+++ dist/types.js
@@ -1,11 +1,6 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.NetworkType = void 0;
-const network_type_1 = require('./src/network-type');
-Object.defineProperty(exports, 'NetworkType', {
-  enumerable: true,
-  get: function () {
-    return network_type_1.NetworkType;
-  },
-});
+const network_type_1 = require("./src/network-type");
+Object.defineProperty(exports, "NetworkType", { enumerable: true, get: function () { return network_type_1.NetworkType; } });
 //# sourceMappingURL=types.js.map
